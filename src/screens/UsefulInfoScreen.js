77293994f08c7bf02f36bc335fbbee61947cc94f
MarkHudson97 <markhudson12@hotmail.com>
2020-07-30 22:30:27 +0100

    More styles and more useful info screen data

--- src/screens/UsefulInfoScreen.js
+++ src/screens/UsefulInfoScreen.js
@@ -10,11 +10,17 @@ const Useful = () => {
         notMobileText: {
             fontSize: 15, marginLeft: "auto", marginRight: "auto", width: "38em"
         },
+        notMobileTextCentered: {
+            fontSize: 15, marginLeft: "auto", marginRight: "auto", width: "38em", textAlign: "center"
+        },
         positioning: {
             display: 'flex', justifyContent: 'center', alignItems: 'center'
         },
-        linkPositioning: {
+        notMobileLinkPositioning: {
             display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none', fontSize: 20
+        },
+        mobileLinkPositioning: {
+            display: 'flex', justifyContent: 'center', textDecoration: 'none', fontSize: 15, textAlign: 'center'
         }
     }
     return (
@@ -24,33 +30,83 @@ const Useful = () => {
                     Useful Information
                 </h1>
 
+                <h2 style={style.positioning}>
+                    What is EDT and how does it work?
+                </h2>
+
                 <Hidden xsDown>
+                    <p style={style.notMobileText}>Essential Driver Training (EDT) was introduced to help students learn vital driving skills and to improve their knowledge and understanding of road safety post doing their theory test.
+                     The course consists of 12, 1 hour lessons or 6 two hour lessons.</p>
+
+                    <p style={style.notMobileText}>Each lesson has a set of expected outcomes describing what you should be able to do by the end of said lesson.
+                      It is important to remember that the EDT course alone is not enough to make you a competent driver and additional lessons and practice with your instructor or a qualified driver with two years experience can be used to bring your skills to a suitable level before progressing on to the next EDT course lesson.</p>
+
+                    <p style={style.notMobileText}>Each lesson has a set of expected outcomes describing what you should be able to do by the end of said lesson.
+                      It is important to remember that the EDT course alone is not enough to make you a competent driver and additional lessons and practice with your instructor or a qualified driver with two years experience can be used to bring your skills to a suitable level before progressing on to the next EDT course lesson.</p>
+
+                    <ol style={style.notMobileTextCentered}>Essential Driver Training Syllabus</ol>
+                    <ol style={style.notMobileTextCentered}>Lesson 1:   Car Controls & Safety Checks</ol>
+                    <ol style={style.notMobileTextCentered}>Lesson 2:   Correct Positioning</ol>
+                    <ol style={style.notMobileTextCentered}>Lesson 3:   Changing Direction</ol>
+                    <ol style={style.notMobileTextCentered}>Lesson 4:   Progression Management</ol>
+                    <ol style={style.notMobileTextCentered}>Lesson 5:   Correct Positioning</ol>
+                    <ol style={style.notMobileTextCentered}>Lesson 6:   Anticipation & Reaction</ol>
+                    <ol style={style.notMobileTextCentered}>Lesson 7:   Sharing the Road</ol>
+                    <ol style={style.notMobileTextCentered}>Lesson 8:   Driving Safely Through Traffic</ol>
+                    <ol style={style.notMobileTextCentered}>Lesson 9:   Changing Direction (More Complex Situations)</ol>
+                    <ol style={style.notMobileTextCentered}>Lesson 10: Speed Management</ol>
+                    <ol style={style.notMobileTextCentered}>Lesson 11: Driving Calmly</ol>
+                    <ol style={style.notMobileTextCentered}>Lesson 12: Night Driving</ol>
+
                     <p style={style.notMobileText}><b><i>Here are some useful links to learn more about the steps you will be taking</i></b></p>
 
-                    <a style={style.linkPositioning} href="https://www.rsa.ie/RSA/Learner-Drivers/Your-learner-permit/The-theory-test/" >Find Information About The Theory Test Here!</a>
+                    <a style={style.notMobileLinkPositioning} href="https://www.rsa.ie/RSA/Learner-Drivers/Your-learner-permit/The-theory-test/" target="_blank">Find Information About The Theory Test Here!</a>
 
-                    <a style={style.linkPositioning} href="https://theorytest.ie/" >Book Your Theory Test Here!</a>
+                    <a style={style.notMobileLinkPositioning} href="https://theorytest.ie/" target="_blank">Book Your Theory Test Here!</a>
 
-                    <a style={style.linkPositioning} href="https://www.rsa.ie/en/RSA/Learner-Drivers/The-Driving-Test/Apply-online/" >Book Your Driving Test Here!</a>
+                    <a style={style.notMobileLinkPositioning} href="https://www.rsa.ie/en/RSA/Learner-Drivers/The-Driving-Test/Apply-online/" target="_blank">Book Your Driving Test Here!</a>
 
-                    <a style={style.linkPositioning} href="https://www.ndls.ie/" >Visit The National Driving Licence Service Here!</a>
+                    <a style={style.notMobileLinkPositioning} href="https://www.ndls.ie/" target="_blank" >Visit The National Driving Licence Service Here!</a>
 
-                    <a style={style.linkPositioning} href="https://www.rsa.ie/en/RSA/Learner-Drivers/Driver-Training/Car-Training-EDT/Learning-to-drive-videos/" >Watch Some Learning To Drive Videos By The RSA Here!</a>
+                    <a style={style.notMobileLinkPositioning} href="https://www.rsa.ie/en/RSA/Learner-Drivers/Driver-Training/Car-Training-EDT/Learning-to-drive-videos/" target="_blank">Watch Some Learning To Drive Videos By The RSA Here!</a>
 
                 </Hidden>
 
                 <Hidden smUp>
+                <p style={style.mobileText}>Essential Driver Training (EDT) was introduced to help students learn vital driving skills and to improve their knowledge and understanding of road safety post doing their theory test.
+                     The course consists of 12, 1 hour lessons or 6 two hour lessons.</p>
+
+                    <p style={style.mobileText}>Each lesson has a set of expected outcomes describing what you should be able to do by the end of said lesson.
+                      It is important to remember that the EDT course alone is not enough to make you a competent driver and additional lessons and practice with your instructor or a qualified driver with two years experience can be used to bring your skills to a suitable level before progressing on to the next EDT course lesson.</p>
+
+                    <p style={style.mobileText}>Each lesson has a set of expected outcomes describing what you should be able to do by the end of said lesson.
+                      It is important to remember that the EDT course alone is not enough to make you a competent driver and additional lessons and practice with your instructor or a qualified driver with two years experience can be used to bring your skills to a suitable level before progressing on to the next EDT course lesson.</p>
+
+                    <ol style={style.mobileText}>Essential Driver Training Syllabus</ol>
+                    <ol style={style.mobileText}>Lesson 1:   Car Controls & Safety Checks</ol>
+                    <ol style={style.mobileText}>Lesson 2:   Correct Positioning</ol>
+                    <ol style={style.mobileText}>Lesson 3:   Changing Direction</ol>
+                    <ol style={style.mobileText}>Lesson 4:   Progression Management</ol>
+                    <ol style={style.mobileText}>Lesson 5:   Correct Positioning</ol>
+                    <ol style={style.mobileText}>Lesson 6:   Anticipation & Reaction</ol>
+                    <ol style={style.mobileText}>Lesson 7:   Sharing the Road</ol>
+                    <ol style={style.mobileText}>Lesson 8:   Driving Safely Through Traffic</ol>
+                    <ol style={style.mobileText}>Lesson 9:   Changing Direction (More Complex Situations)</ol>
+                    <ol style={style.mobileText}>Lesson 10: Speed Management</ol>
+                    <ol style={style.mobileText}>Lesson 11: Driving Calmly</ol>
+                    <ol style={style.mobileText}>Lesson 12: Night Driving</ol>
+
                     <p style={style.mobileText}><b><i>Here are some useful links to learn more about the steps you will be taking</i></b></p>
 
-                    <a style={style.linkPositioning} href="https://www.rsa.ie/RSA/Learner-Drivers/Your-learner-permit/The-theory-test/" >Information about the Theory Test Here!</a>
-
-                    <a style={style.linkPositioning} href="https://theorytest.ie/" >Book Your Theory Test Here!</a>
-
-                    <a style={style.linkPositioning} href="https://www.rsa.ie/en/RSA/Learner-Drivers/The-Driving-Test/Apply-online/" >Book Your Driving Test Here!</a>
-
-                    <a style={style.linkPositioning} href="https://www.ndls.ie/" >Visit the National Driving Licence Service Here!</a>
-
-                    <a style={style.linkPositioning} href="https://www.rsa.ie/en/RSA/Learner-Drivers/Driver-Training/Car-Training-EDT/Learning-to-drive-videos/" >Watch some Learning To Drive videos by the RSA Here!</a>
+                    <a style={style.mobileLinkPositioning} href="https://www.rsa.ie/RSA/Learner-Drivers/Your-learner-permit/The-theory-test/" target="_blank">Find Information About The Theory Test Here!</a>
+                    <br />
+                    <a style={style.mobileLinkPositioning} href="https://theorytest.ie/" target="_blank">Book Your Theory Test Here!</a>
+                    <br />
+                    <a style={style.mobileLinkPositioning} href="https://www.rsa.ie/en/RSA/Learner-Drivers/The-Driving-Test/Apply-online/" target="_blank">Book Your Driving Test Here!</a>
+                    <br />
+                    <a style={style.mobileLinkPositioning} href="https://www.ndls.ie/" target="_blank">Visit The National Driving Licence Service Here!</a>
+                    <br />
+                    <a style={style.mobileLinkPositioning} href="https://www.rsa.ie/en/RSA/Learner-Drivers/Driver-Training/Car-Training-EDT/Learning-to-drive-videos/" target="_blank">Watch Some Learning To Drive Videos By The RSA Here!</a>
                 </Hidden>
 
             </div>
